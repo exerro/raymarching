b054e1cc9e1c64f11fb1d0b8a36e32a879076718
exerro <benedict.allen2514@gmail.com>
2019-05-12 00:49:33 +0100

    Improve display callback handling

--- 3d/src/Main.kt
+++ 3d/src/Main.kt
@@ -2,8 +2,6 @@ import display.Display
 import gl.loadShaderProgram
 import org.lwjgl.glfw.GLFW.*
 import shape.*
-import util.mat4_identity
-import util.mat4_scale
 import util.vec3
 import util.vec4
 
@@ -14,11 +12,9 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        assert(mat4_identity.mul(mat4_scale(2.0f)) == mat4_scale(2.0f))
         val display = Display(WIDTH, HEIGHT)
-
         val compiler = ShaderCompiler()
-        val c1 = Sphere(vec4(-4f, 2f, 0f, 0f), 8f)
+        val sphere = Sphere(vec4(-4f, 2f, 0f, 0f), 8f)
         val blend: ShapeBlend = blendOfShapes(
                 2f,
                 Sphere(vec4(4f, -3f, 0f, 0f), 6f),
@@ -27,61 +23,69 @@ object Main {
                 Line(vec4(0.0f, 0.0f, 5.0f, 0.0f), vec4(15.0f, 15.0f, -15.0f, 0.0f), 2.0f),
                 Box(vec4(-10.0f, -10.0f, 5.0f, 0.0f), vec3(5.0f, 5.0f, 5.0f))
         ) as ShapeBlend
-        var shape: Shape = dissolveOfShapes(
+        val shape: Shape = dissolveOfShapes(
                 2f,
                 blend,
-                c1
+                sphere
         )
         val vertexShader = compiler.buildVertexShader()
         val fragmentShader = compiler.buildFragmentShader(shape)
+        var renderer: ShapeRenderer? = null
 
-        println(vertexShader)
-        println(fragmentShader)
+        val speed = 50f
 
-        val renderer = ShapeRenderer(
-                WIDTH.toFloat()/HEIGHT,
-                loadShaderProgram(vertexShader, fragmentShader),
-                shape,
-                compiler.uniformValueLookups
-        )
+        display.onMouseDownCallback = { _, _ ->
+            display.setMouseLocked(true)
+        }
+
+        display.onMouseUpCallback = { _, _ ->
+            if (!display.isMouseDown()) display.setMouseLocked(false)
+        }
+
+        display.onMouseDragCallback = { pos, last, _, _ ->
+            val dx = pos.x - last.x
+            val dy = pos.y - last.y
+            renderer!!.rotateY(-dx / display.width * 0.5f)
+            renderer!!.rotateX(-dy / display.height * 0.5f)
+        }
+
+        display.onLoadCallback = {
+            renderer = ShapeRenderer(
+                    WIDTH.toFloat()/HEIGHT,
+                    loadShaderProgram(vertexShader, fragmentShader),
+                    shape,
+                    compiler.uniformValueLookups
+            )
+        }
 
         display.onResizedCallback = { width, height ->
-            renderer.aspectRatio = width.toFloat() / height
+            renderer!!.aspectRatio = width.toFloat() / height
         }
 
-        glfwSetInputMode(display.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
+        display.onUpdateCallback = { dt ->
+            if (display.isKeyDown(GLFW_KEY_W)) renderer!!.forward(speed * dt)
+            if (display.isKeyDown(GLFW_KEY_S)) renderer!!.forward(-speed * dt)
+            if (display.isKeyDown(GLFW_KEY_A)) renderer!!.right(-speed * dt)
+            if (display.isKeyDown(GLFW_KEY_D)) renderer!!.right(speed * dt)
+            if (display.isKeyDown(GLFW_KEY_SPACE)) renderer!!.up(speed * dt)
+            if (display.isKeyDown(GLFW_KEY_LEFT_SHIFT)) renderer!!.up(-speed * dt)
 
-        var lastX = 0f
-        var lastY = 0f
-        val speed = 0.8f
-
-        glfwSetCursorPosCallback(display.window) { window, x, y ->
-            val dx = (x - lastX).toFloat()
-            val dy = (y - lastY).toFloat()
-            lastX = x.toFloat()
-            lastY = y.toFloat()
-            renderer.rotateY(-dx / display.width * 0.5f)
-            renderer.rotateX(-dy / display.height * 0.5f)
-        }
-
-        display.loop {
-            if (display.isKeyDown(GLFW_KEY_W)) renderer.forward(speed)
-            if (display.isKeyDown(GLFW_KEY_S)) renderer.forward(-speed)
-            if (display.isKeyDown(GLFW_KEY_A)) renderer.right(-speed)
-            if (display.isKeyDown(GLFW_KEY_D)) renderer.right(speed)
-            if (display.isKeyDown(GLFW_KEY_SPACE)) renderer.up(speed)
-            if (display.isKeyDown(GLFW_KEY_LEFT_SHIFT)) renderer.up(-speed)
-
-             c1.setPosition(vec4(0.0f, Math.sin(t.toDouble() * 3).toFloat() * 8, 0.0f, 0.0f))
+            sphere.setPosition(vec4(0.0f, Math.sin(t.toDouble() * 3).toFloat() * 8, 0.0f, 0.0f))
             val f = 1 + Math.sin(t.toDouble() * 10).toFloat() * 3
             blend.setFactor(f)
             (blend.a as ShapeBlend).setFactor(f)
             (blend.a.a as ShapeBlend).setFactor(f)
             (blend.a.a.a as ShapeBlend).setFactor(f)
-            renderer.renderToScreen()
-            t += 0.01f
+
+            println("FPS: ${1/dt}")
+
+            t += dt
         }
 
-        display.close()
+        display.onDrawCallback = {
+            renderer!!.renderToScreen()
+        }
+
+        display.run()
     }
 }
--- 3d/src/display/Display.kt
+++ 3d/src/display/Display.kt
@@ -5,109 +5,106 @@ import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11.*
-import org.lwjgl.system.MemoryStack.stackPush
 import org.lwjgl.system.MemoryUtil.NULL
 import gl.GLResource
+import util.vec2
 
 class Display(var width: Int, var height: Int, val title: String = "Display") : GLResource {
-    val window: Long
+    private var running = false
+    private val heldMouseButtons = HashSet<Int>()
+    private var lastMousePosition = vec2(0f, 0f)
+    private var firstMousePosition = vec2(0f, 0f)
+    private var onSetup: MutableList<() -> Unit> = ArrayList()
+    private var setup = false
+    var window: Long = 0
     var onResizedCallback: ((Int, Int) -> Unit)? = null
-    var onMouseDownCallback: ((Int, Int) -> Unit)? = null
-    var onMouseUpCallback: ((Int, Int) -> Unit)? = null
-    var onMouseMoveCallback: ((Int, Int, Int, Int) -> Unit)? = null
-    var onMouseDragCallback: ((Int, Int, Int, Int, Int, Int) -> Unit)? = null
-    var onKeyPressedCallback: ((Int) -> Unit)? = null
-    var onKeyReleasedCallback: ((Int) -> Unit)? = null
+    var onMouseDownCallback: ((vec2, Int) -> Unit)? = null
+    var onMouseUpCallback: ((vec2, Int) -> Unit)? = null
+    var onMouseMoveCallback: ((vec2, vec2) -> Unit)? = null
+    var onMouseDragCallback: ((vec2, vec2, vec2, Set<Int>) -> Unit)? = null
+    var onKeyPressedCallback: ((Int, Int) -> Unit)? = null
+    var onKeyReleasedCallback: ((Int, Int) -> Unit)? = null
+    var onTextInputCallback: ((String) -> Unit)? = null
+    var onUpdateCallback: ((Float) -> Unit)? = null
+    var onDrawCallback: (() -> Unit)? = null
+    var onLoadCallback: (() -> Unit)? = null
 
-    /**
-     * Creates a new display, using the standard GLFW init code found at https://www.lwjgl.org/guide
-     */
-    init {
-        GLFWErrorCallback.createPrint(System.err).set()
-
-        if(!glfwInit())
-            throw IllegalStateException("Unable to initialize GLFW")
-
-        // configure GLFW
-        glfwDefaultWindowHints() // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
-        glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_TRUE) // the window will focus when shown
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
-        glfwWindowHint(GLFW_CENTER_CURSOR , GLFW_TRUE) // the window will have the cursor centred
-
-        // create the window
-        window = glfwCreateWindow(width, height, title, NULL, NULL)
-
-        if (window == NULL)
-            throw RuntimeException("Failed to create the GLFW window")
-
-        // get the thread stack and push a new frame
-        stackPush().use { stack ->
-            val pWidth = stack.mallocInt(1) // int*
-            val pHeight = stack.mallocInt(1) // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight)
-
-            // get the resolution of the primary monitor
-            val videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor())
-
-            // center the window
-            if (videoMode != null)
-                glfwSetWindowPos(
-                        window,
-                        (videoMode.width() - pWidth.get(0)) / 2,
-                        (videoMode.height() - pHeight.get(0)) / 2
-                )
-        }
-
-        // make the OpenGL context current
-        glfwMakeContextCurrent(window)
-        // enable v-sync
-        glfwSwapInterval(1)
-
-        // make the window visible
-        glfwShowWindow(window)
-
-        // "This line is critical for LWJGL's inter-operation with GLFW's
-        //  OpenGL context, or any context that is managed externally.
-        //  LWJGL detects the context that is current in the current thread,
-        //  creates the GLCapabilities instance and makes the OpenGL
-        //  bindings available for use."
-        GL.createCapabilities()
-
-        glfwSetWindowSizeCallback(window) { _, _, _ ->
-            updateGLViewport()
-            onResizedCallback?.invoke(width, height)
+    fun setMouseLocked(locked: Boolean) {
+        whenSetup {
+            glfwSetInputMode(window, GLFW_CURSOR, if (locked) GLFW_CURSOR_DISABLED else GLFW_CURSOR_NORMAL)
         }
     }
 
+    /**
+     * Return true if the key given is currently held
+     */
     fun isKeyDown(key: Int): Boolean = glfwGetKey(window, key) == GLFW_PRESS
 
-    fun loop(render: () -> Unit) {
-        // set the clear colour to black
-        updateGLViewport()
+    /**
+     * Return true if the mouse button given is currently held
+     */
+    fun isMouseDown(button: Int): Boolean = glfwGetMouseButton(window, button) == GLFW_PRESS
+
+    /**
+     * Return true if any mouse button is currently held
+     */
+    fun isMouseDown(): Boolean = heldMouseButtons.isNotEmpty()
+
+    /**
+     * Gets the mouse position
+     */
+    fun getMousePosition(): vec2 {
+        val w = DoubleArray(1)
+        val h = DoubleArray(1)
+        glfwGetCursorPos(window, w, h)
+        return vec2(w[0].toFloat(), h[0].toFloat())
+    }
+
+    /**
+     * Gets the size of the window
+     */
+    fun getWindowSize(): vec2 {
+        val w = IntArray(1)
+        val h = IntArray(1)
+        glfwGetWindowSize(window, w, h)
+        return vec2(w[0].toFloat(), h[0].toFloat())
+    }
+
+    /**
+     * Closes the display
+     */
+    fun close() {
+        running = false
+    }
+
+    fun run() {
+        var lastUpdate = System.currentTimeMillis()
+
+        setup()
+        running = true
 
         // run the rendering loop until the user has attempted to close the window or has pressed the ESCAPE key
-        while (!glfwWindowShouldClose(window)) {
-            // clear the framebuffer
+        while (running && !glfwWindowShouldClose(window)) {
+            // set the clear colour to black and clear the framebuffer
             glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
             glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
 
-            // call the render callback
-            render()
+            val t = System.currentTimeMillis()
+            val dt = t - lastUpdate
+            lastUpdate = t
+
+            // call the update render callbacks
+            onUpdateCallback?.invoke(dt / 1000f)
+            onDrawCallback?.invoke()
 
             // swap the color buffers to present the content to the screen
             glfwSwapBuffers(window)
 
             // poll for window events
-            // he key callback above will only be invoked during this call
+            // the key callback above will only be invoked during this call
             glfwPollEvents()
         }
-    }
 
-    fun close() {
-        // terminate GLFW and free the error callback
         destroy()
         glfwTerminate()
         glfwSetErrorCallback(null)!!.free()
@@ -119,10 +116,92 @@ class Display(var width: Int, var height: Int, val title: String = "Display") : 
         glfwDestroyWindow(window)
     }
 
+    /**
+     * Creates a new display, using the standard GLFW init code found at https://www.lwjgl.org/guide
+     */
+    private fun setup() {
+        GLFWErrorCallback.createPrint(System.err).set()
+
+        // initialise GLFW
+        if(!glfwInit()) throw IllegalStateException("Unable to initialize GLFW")
+
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
+        glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_TRUE) // the window will focus when shown
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
+        glfwWindowHint(GLFW_CENTER_CURSOR , GLFW_TRUE) // the window will have the cursor centred
+
+        window = glfwCreateWindow(width, height, title, NULL, NULL) // create the window
+        if (window == NULL) throw RuntimeException("Failed to create the GLFW window")
+
+        val videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+        if (videoMode != null)
+            glfwSetWindowPos(
+                    window,
+                    (videoMode.width() - getWindowSize().x.toInt()) / 2,
+                    (videoMode.height() - getWindowSize().y.toInt()) / 2
+            )
+
+        glfwMakeContextCurrent(window) // make the OpenGL context current
+        glfwSwapInterval(1) // enable v-sync
+        glfwShowWindow(window) // make the window visible
+
+        GL.createCapabilities() // makes OpenGL bindings available to use from LWJGL
+
+        setCallbacks() // sets the GLFW callbacks to use the custom callbacks above
+        updateGLViewport() // update the GL viewport to set it up initially
+
+        onLoadCallback?.invoke() // call the loaded callback, if set
+        onSetup.map { it() }
+        setup = true
+    }
+
+    private fun setCallbacks() {
+        // on window resize, update the GL viewport and call a resized callback, if set
+        glfwSetWindowSizeCallback(window) { _, _, _ ->
+            updateGLViewport()
+            onResizedCallback?.invoke(width, height)
+        }
+
+        glfwSetKeyCallback(window) { _, key, _, action, mods ->
+            if (action == GLFW_PRESS) onKeyPressedCallback?.invoke(key, mods)
+            if (action == GLFW_RELEASE) onKeyReleasedCallback?.invoke(key, mods)
+        }
+
+        glfwSetCharCallback(window) { _, codepoint ->
+            onTextInputCallback?.invoke(Character.toChars(codepoint).toString())
+        }
+
+        glfwSetCursorPosCallback(window) { _, _, _ ->
+            val pos = getMousePosition()
+            if (heldMouseButtons.isEmpty()) onMouseMoveCallback?.invoke(pos, lastMousePosition)
+            else onMouseDragCallback?.invoke(pos, lastMousePosition, firstMousePosition, heldMouseButtons)
+            lastMousePosition = pos
+        }
+
+        glfwSetMouseButtonCallback(window) { _, button, action, mods ->
+            val pos = getMousePosition()
+
+            if (action == GLFW_PRESS) {
+                if (heldMouseButtons.isEmpty()) firstMousePosition = pos
+                heldMouseButtons.add(button)
+                onMouseDownCallback?.invoke(pos, mods)
+            }
+            else if (action == GLFW_RELEASE) {
+                heldMouseButtons.remove(button)
+                onMouseUpCallback?.invoke(pos, mods)
+            }
+        }
+    }
+
     private fun updateGLViewport() {
         val width = IntArray(1)
         val height = IntArray(1)
         glfwGetFramebufferSize(window, width, height)
         glViewport(0, 0, width[0], height[0])
+    }
+
+    private fun whenSetup(func: () -> Unit) {
+        if (setup) func()
+        else onSetup.add(func)
     }
 }
