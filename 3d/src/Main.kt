13d8f5320081925d166090e7a3b9ab5e17671584
exerro <benedict.allen2514@gmail.com>
2020-06-24 19:23:54 +0100

    Add torus

--- 3d/src/Main.kt
+++ 3d/src/Main.kt
@@ -7,9 +7,8 @@ import raymarch.RaymarchingShapeRenderer
 import raymarch.RenderOptions
 import shape.*
 import shape.container.*
-import shape.primitive.Box
-import shape.primitive.ShapePlane
-import shape.primitive.Sphere
+import shape.primitive.*
+import shape.primitive.torusInverted
 
 fun boxOutline(size: vec3, colour: vec3 = vec3(1f, 1f, 1f), reflectivity: Float = 0.3f): Shape {
     return ShapeDifference(
@@ -23,12 +22,13 @@ fun boxOutline(size: vec3, colour: vec3 = vec3(1f, 1f, 1f), reflectivity: Float 
 
 object Main {
     var t: Float = 0.0f
+    var ts = 1f
     val WIDTH = 720
     val HEIGHT = 540
 
     @JvmStatic
     fun main(args: Array<String>) {
-//        Logging.enable()
+        Logging.enable()
 
         val display = Display(WIDTH, HEIGHT)
         val sphere = Sphere(8f).setColour(1.0f, 0.5f, 0.5f).setScale(1f).setTranslation(vec3(-4f, 2f, 0f)).setReflectivity(0f)
@@ -114,6 +114,55 @@ object Main {
                 ShapePlane(vec3(0f, -20f, 0f), vec3(0f, 1f, 0f)).setReflectivity(0.3f).setColour(0.9f, 0.3f, 0.6f)
         )
 
+        val torus = Torus(10f, 3f).setColour(1f, 0f, 0f).setReflectivity(0.5f)
+
+//        shape = ShapeUnion(
+//                torus,
+//                Torus(10f, 0.5f).setColour(0f, 1f, 0f).setReflectivity(0.5f),
+//                Sphere(3f).setColour(0f, 0f, 1f).setReflectivity(0.5f),
+//                ShapePlane(vec3(0f, -20f, 0f), vec3(0f, 1f, 0f)).setReflectivity(0.3f).setColour(0.9f, 0.3f, 0.6f)
+//        )
+
+        // Box(vec3(1f)).setColour(0f, 0f, 1f).setReflectivity(0.5f)
+
+        val a = ShapeUnion(
+                torus,
+                Torus(8f, 1f).setColour(1f, 0f, 1f).setReflectivity(0.5f).setTranslation(vec3(0f, 10f, 0f)),
+                Sphere(2f).setColour(1f, 1f, 0f).setReflectivity(0f).setTranslation(vec3(0f, 5f, 0f))
+        )
+        val b = ShapeUnion(
+                ShapeDifference(
+                        ShapeDifference(
+                                ShapeDissolve(1f,
+                                        ShapeBlend(0.7f,
+                                                Box(vec3(10f)).setTranslation(vec3(1f, 3f, 0f)).setColour(0f, 0f, 1f).setReflectivity(0.3f),
+                                                Sphere(2f).setTranslation(vec3(5f, 10.5f, 9.5f)).setColour(1f, 1f, 0f).setReflectivity(0.5f),
+                                                Sphere(2f).setTranslation(vec3(-3f, 10.5f, 9.5f)).setColour(1f, 1f, 0f).setReflectivity(0.5f)
+                                        ),
+                                        Box(vec3(8f)).setTranslation(vec3(1f, 8.1f, 0f)).setReflectivity(0.7f).setColour(0.4f, 0.4f, 1f)
+                                ),
+                                Box(vec3(11f, 2f, 11f)).setTranslation(vec3(1f, 6f, 0f)).setReflectivity(0.9f)
+                        ),
+                        Sphere(14f).setTranslation(vec3(-10f, 15f, 10f))
+                ),
+                Sphere(1.5f).setTranslation(vec3(-3f, 3f, -4f)).setColour(1f, 1f, 0f).setReflectivity(0.5f),
+                Sphere(1.5f).setTranslation(vec3(-3f, 3f, 4f)).setColour(1f, 1f, 0f).setReflectivity(0.5f),
+                Sphere(1.5f).setTranslation(vec3(5f, 3f, -4f)).setColour(1f, 1f, 0f).setReflectivity(0.5f),
+                Sphere(1.5f).setTranslation(vec3(5f, 3f, 4f)).setColour(1f, 1f, 0f).setReflectivity(0.5f),
+                ShapeBlend(0.8f,
+                        Sphere(3f).setTranslation(vec3(1f, 8f, 0f)).setColour(1f, 0f, 0f).setReflectivity(0.3f),
+                        Sphere(2f).setTranslation(vec3(1f, 3f, 0f)).setColour(1f, 1f, 0f).setReflectivity(0.5f),
+                        Torus(3.9f, 1.2f).setTranslation(vec3(1f, 8f, 0f)).setColour(1f, 0f, 1f).setReflectivity(0.7f)
+                )
+        )
+        val tt = ShapeTransition(a, b, 0.5f)
+        tt.getTransitionProperty().setDynamic(true)
+
+//        shape = ShapeUnion(
+//                tt,
+//                ShapePlane(vec3(0f, -20f, 0f), vec3(0f, 1f, 0f)).setReflectivity(0.3f).setColour(0.9f, 0.3f, 0.6f)
+//        )
+
 //        shape = ShapeBlend(
 //                1.7f,
 //                *stuff,
@@ -191,6 +240,14 @@ object Main {
                 advanceTime = !advanceTime
             }
 
+            if (key == GLFW_KEY_P) {
+                torus.setInverted(-torus.getInverted())
+            }
+
+            if (key == GLFW_KEY_T) {
+                ts = if (ts == 1f) 0.2f else 1f
+            }
+
             if (set) renderer.loadShape(shape, options)
         }
 
@@ -203,6 +260,8 @@ object Main {
             if (display.isKeyDown(GLFW_KEY_D)) renderer.camera.moveBy(renderer.camera.flatRight * speed * dt)
             if (display.isKeyDown(GLFW_KEY_SPACE)) renderer.camera.moveBy(renderer.camera.flatUp * speed * dt)
             if (display.isKeyDown(GLFW_KEY_LEFT_SHIFT)) renderer.camera.moveBy(renderer.camera.flatUp * -speed * dt)
+
+            tt.setTransition(0.5f + Math.sin(t.toDouble()).toFloat() * 0.5f)
 
             sphere.setTranslation(vec3(0.0f, Math.sin(t.toDouble() * 3).toFloat() * 8 + 5, 0.0f))
             sphere2.setTranslation(vec3(0.0f, Math.sin(t.toDouble() * 3).toFloat() * 8 + 5, 0.0f))
@@ -225,13 +284,14 @@ object Main {
 //                child.setTranslation(vec3(child.getPosition().x, child.getPosition().y, Math.sin((t * 3 + child.getPosition().x * 5 + child.getPosition().y * 3).toDouble()).toFloat()))
 //            }
 
-            t += dt * (if (advanceTime) 1 else 0)
+            t += dt * (if (advanceTime) 1 else 0) * ts
         }
 
         display.attachDrawCallback {
             renderer.renderToFramebuffer()
             context2D.drawTexture(renderer.getTexture(), vec2(0f, 0f))
             context2D.write(display.fps.toString(), font, vec2(0f))
+            context2D.write("${(0.5f + Math.sin(t.toDouble()).toFloat() * 0.5f * 100f).toInt() / 100f} / ${(t * 100f).toInt() / 100f}", font, vec2(0f, 80f))
 //            buffer?.debugDraw()
         }
 
